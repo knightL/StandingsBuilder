6d757aaf053d2304508925505203c72cf021e8ed
Ляшко Александр Дмитриевич <knightl@knightl-notebook>
2017-10-22 13:24:51 +0300

    Fixed NYParser: now it succesfully processes all New York Region standings. A warning is printed if there is an empty row between rank and first team.

--- src/Parsers/NYParser.cpp
+++ src/Parsers/NYParser.cpp
@@ -11,6 +11,7 @@
 #include <cstring>
 #include <cassert>
 #include <cstdlib>
+#include <ctype.h>
 #include <algorithm>
 using namespace std;
 
@@ -26,24 +27,61 @@ NYParser::NYParser(const XMLParser& config, xmlNodePtr start):EventBasedParser(c
 
 	if(reader.getType()!=FileReader::None)
 	{
-		//read standings into memory
-		const char* buf=reader.read();
-		if(buf)
+		//read standings into 
+		const char* inp=reader.read();
+		if(inp)
 		{
-
+			char *buf=(char*)calloc(strlen(inp)+1, sizeof(char));
+			for(int i=0;inp[i];i++)
+				buf[i]=inp[i];
+			
+			// tidy up the document before processing
+			bool first_html=true;
 			int top=0;
 			int len=strlen(buf);
-			// skip till HTML tag
+			
+			// skip till HTML tag and remove multiple instances of it
 			for(int i=0;i<len;i++)
 			{
-				if(!strncmp(&buf[i],"<html>",6))
+				const char* HTML="<html>";
+				bool has_html=true;
+				for(int j=0;j<6 && has_html;j++)
+					has_html &= tolower(buf[i+j]) == HTML[j];
+				if(has_html)
 				{
-					top=i;
-					break;
+					if(first_html)
+					{
+						top=i;
+						first_html=false;
+					}
+					else
+					{
+						for(int j=0;j<6;j++)
+							buf[i+j]=' ';
+					}
 				}
 			}
+			
+			//delete uppercase tags <BODY>, <HEAD> and </BODY>, </HEAD>, </HTML> as they are duplicates of already existing
+			char dup_tags[5][8]={"<BODY>", "<HEAD>", "</BODY>", "</HEAD>", "</HTML>"};
+			int dup_len[5];
+			for(int i=0;i<5;i++)
+				dup_len[i]=strlen(dup_tags[i]);
+			
+			for(int i=0;i<len;i++)
+			{
+				for(int j=0;j<5;j++)
+				{
+					if( strncmp(&buf[i], dup_tags[j], dup_len[j]) == 0 )
+						for(int k=0;k<dup_len[j];k++)
+							buf[i+k]=' ';
+				}
+			}
+			
+			
+			// process document and go to the first row. it should be skipped as it is the header
 			XMLParser xml((xmlDocPtr)htmlReadMemory(buf+top,len-top,NULL,NULL,0));
-			xmlNodePtr start=xml.walkPath(xml.getRoot(), {">", "body", ">", "table", ">", "tbody", ">", "tr"});
+			xmlNodePtr start=xml.walkPath(xml.getRoot(), {">", "body", ">", "table", ">", "tr"});
 			while(start)
 			{
 				start=xml.findNode(xml.getNext(start),"tr");
@@ -51,17 +89,22 @@ NYParser::NYParser(const XMLParser& config, xmlNodePtr start):EventBasedParser(c
 				xmlNodePtr cur=xml.getChild(start);
 				if(!cur) break;
 				if(xml.findNode(cur, "th") != NULL) break;
-				cur=xml.getNext(xml.findNode(cur,"td"));
+				// skip rank
+				cur=xml.findNode(xml.getNext(xml.findNode(cur,"td")), "td");
 				if(!cur) break;
 				// read information about team and store it as events
-				cur=xml.findNode(cur,"td");
 				string name=xml.getNodeContent(cur);
 				name=prefix+name;
-				cur=xml.getNext(xml.findNode(cur,"td"));
-				cur=xml.getNext(xml.findNode(cur,"td"));
+				cur=xml.findNode(xml.getNext(cur),"td");
+				cur=xml.findNode(xml.getNext(cur),"td");
 				for(int i=0;i<problem_count;i++)
 				{
 					cur=xml.getNext(xml.findNode(cur,"td"));
+					if(!cur)
+					{
+						fprintf(stderr,"NYParser: incomplete table row found\n");
+						break;
+					}
 					string t1=xml.getNodeContent(cur);
 					for(int j=0;j<t1.size();j++)
 						if(t1[j]=='/')
@@ -78,6 +121,7 @@ NYParser::NYParser(const XMLParser& config, xmlNodePtr start):EventBasedParser(c
 						this->add_event(tac, name, i, true, acnt-1);
 				}
 			}
+			free(buf);
 		}
 	}
 }
@@ -97,7 +141,8 @@ std::string NYParser::getName()
 
 std::string NYParser::getDescription()
 {
-	return "Necessary attributes:\n"
+	return "New York Contest Parser\n"
+	    "Necessary attributes:\n"
 		"<Path> - path to new york standings\n"
 #ifdef HAVE_LIBCURL
 		" or\n"
